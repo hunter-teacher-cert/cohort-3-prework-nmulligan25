7f071343cd8d9e4bdba9a1e48ebdcf1c9c99bb31
Nora <96584627+nmulligan25@users.noreply.github.com>
2022-02-27 20:00:29 +0000

    added Beer.java

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -2,11 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Beer{
-
+  public static double
+public static void main(String[] args){
+  
 public static void countdown(int n) {
     if (n == 99) {
         System.out.println( n, "Bottles of beer on the wall");
+     
     } else {
         System.out.println(n);
         countdown(n - 1);
     }
+  System.out.println( n, "Bottles of beer on the wall");
+      System.out.println( "ya take one down, ya pass it around");
+  }
+}
+
+
+
+  
