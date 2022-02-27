6e262177a5102581841f93afbb7f4ec3c2e350da
Nora <96584627+nmulligan25@users.noreply.github.com>
2022-02-27 17:00:03 +0000

    Added pre04 Conditional.java

---
+++ pre04/Conditional.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Conditional{
+
+  public static void main (String [] 
+  args){
+
+    int a;
+
+    a = 10;
+
+    if (a >10){
+      System.out.println("It is bigger");
+      System.out.println("in if");
+    }else if (a < 10)
+    {
+      System.out.println("It is < 10.");
+      }
+    else {
+      System.out.println("It is = 10.");
+    }
+    
+      System.out.println("Not in if");
+    
+    
+  
+    }
+  
+}
---
+++ pre04/answer1.java
@@ -0,0 +1,5 @@
+if (x > 0) {
+    if (x < 10) {
+        System.out.println("positive single digit number.");
+    }
+}
